c2ead80cef9f3a2c92862e7e8603feeac1e5fd15
kimtaehan11 <kimtaehan11@gmail.com>
2023-10-18 17:20:03 +0900

    first commit

---
+++ src/main/java/org/developx/effective_java/EffectiveJavaApplication.java
@@ -0,0 +1,13 @@
+package org.developx.effective_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffectiveJavaApplication.class, args);
+	}
+
+}
